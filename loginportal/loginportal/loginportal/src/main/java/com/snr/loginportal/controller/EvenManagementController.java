95c626a74879566fe064665835b65bd02c098621
ShreeJananiMK <124903285+ShreeJananiMK@users.noreply.github.com>
2025-02-26 08:38:21 +0530

    Update EvenManagementController.java

--- loginportal/loginportal/loginportal/src/main/java/com/snr/loginportal/controller/EvenManagementController.java
+++ loginportal/loginportal/loginportal/src/main/java/com/snr/loginportal/controller/EvenManagementController.java
@@ -11,10 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-
 import java.util.Map;
 
+
+@CrossOrigin(origins = "*", allowedHeaders = "*", methods = { RequestMethod.POST, RequestMethod.GET, RequestMethod.PUT, RequestMethod.DELETE })
 @RestController
 @RequestMapping("/api")
 public class EvenManagementController {
