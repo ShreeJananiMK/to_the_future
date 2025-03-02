34edbb2ef0b35c52d486c2c637946fe35000dfa6
ShreeJananiMK <124903285+ShreeJananiMK@users.noreply.github.com>
2025-03-02 20:24:19 +0530

    Update StudentRegistrationServiceImpl.java

--- loginportal/loginportal/loginportal/src/main/java/com/snr/loginportal/service/implement/StudentRegistrationServiceImpl.java
+++ loginportal/loginportal/loginportal/src/main/java/com/snr/loginportal/service/implement/StudentRegistrationServiceImpl.java
@@ -1,12 +1,14 @@
 package com.snr.loginportal.service.implement;
 
 import com.snr.loginportal.config.WebConfig;
+import com.snr.loginportal.dto.CertificateDetails;
 import com.snr.loginportal.dto.StatusProjection;
 import com.snr.loginportal.model.EventManagement;
 import com.snr.loginportal.model.StudentRegistration;
 import com.snr.loginportal.repository.StudentRegistrationRepo;
 import com.snr.loginportal.service.EventManagementService;
 import com.snr.loginportal.service.StudentRegistrationService;
+import com.snr.loginportal.util.CertificateGeneration;
 import jakarta.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,6 +17,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.awt.*;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -69,13 +74,33 @@ public class StudentRegistrationServiceImpl implements StudentRegistrationServic
 
     @Transactional
     @Override
-    public String updateStatus(Long id, String status) {
+    public String updateStatus(Long id, String status) throws Exception {
         studentRegistrationRepo.updateStatus(id, status);
-        return "Updated successfully";
+        if(status.equalsIgnoreCase("approved")) {
+            StudentRegistration result = studentRegistrationRepo.findById(id).get();
+            logger.info("The Input student info  : ---- : {}", result);
+            CertificateDetails certificateDetails = new CertificateDetails();
+            certificateDetails.setStudentName(result.getStudentName());
+            certificateDetails.setCollegeName(result.getCollegeName());
+            certificateDetails.setEventName(result.getEventName());
+            certificateDetails.setCoordinatorName(result.getEventId().getEventCoordinator());
+            certificateDetails.setDate((result.getEventId().getCreatedAt()).format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+            certificateDetails.setEmail(result.getEmail());
+            logger.info("The certificate info  : ---- : {}", certificateDetails);
+            generateAndSendCertificates(certificateDetails);
+            return "Certificate is sent successfully";
+        }
+        else return "Certificate is not sent to the participant";
     }
 
     @Override
     public StatusProjection getStatus() {
       return studentRegistrationRepo.getStatus();
+    }
+
+    @Override
+    public void generateAndSendCertificates(CertificateDetails certificateDetails) throws Exception{
+        CertificateGeneration certificateGeneration = new CertificateGeneration();
+        certificateGeneration.generateAndSendCertificates(certificateDetails);
     }
 }
