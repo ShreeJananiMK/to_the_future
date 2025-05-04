97e97f6fd7c209431e403ac789baa2a8d79c0300
ShreeJananiMK <124903285+ShreeJananiMK@users.noreply.github.com>
2025-05-04 17:17:10 +0530

    Update StudentRegistrationController.java

--- loginportal/loginportal/loginportal/src/main/java/com/snr/loginportal/controller/StudentRegistrationController.java
+++ loginportal/loginportal/loginportal/src/main/java/com/snr/loginportal/controller/StudentRegistrationController.java
@@ -1,24 +1,25 @@
 package com.snr.loginportal.controller;
 
+import com.snr.loginportal.dto.RegistrationCountProjection;
 import com.snr.loginportal.dto.StatusProjection;
+import com.snr.loginportal.dto.StudentRegistrationCount;
 import com.snr.loginportal.model.StudentRegistration;
+import com.snr.loginportal.repository.StudentRegistrationRepo;
 import com.snr.loginportal.service.EventManagementService;
 import com.snr.loginportal.service.StudentRegistrationService;
-import com.snr.loginportal.service.implement.EventManagementServiceImpl;
 import com.snr.loginportal.util.ApiResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*", methods = { RequestMethod.POST, RequestMethod.GET, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.PATCH })
 @RestController
@@ -36,7 +37,6 @@ public class StudentRegistrationController {
 
     private final Logger logger = LoggerFactory.getLogger(StudentRegistrationController.class);
     @PostMapping("/registrations")
-     @PostMapping("/registrations")
     public ResponseEntity<?> addStudentDetails(@RequestBody StudentRegistration studentRegistration){
         if(studentRegistration != null && (studentRegistrationRepo.checkStudentCount(studentRegistration.getEventName())<=60)){
             logger.info("The Input data : ---- : {}",studentRegistration);
@@ -60,7 +60,7 @@ public class StudentRegistrationController {
         return new ResponseEntity<>(new ApiResponse(HttpStatus.OK, result), HttpStatus.OK);
     }
 
-   @PatchMapping("/registrations/{id}/status")
+    @PatchMapping("/registrations/{id}/status")
     public ResponseEntity<?> updateStatus(@PathVariable("id") Long id, @RequestParam String status) throws Exception{
         if(studentRegistrationService.studentExists(id)) {
             logger.info("Student Id : ---- : {}",studentRegistrationService.studentExists(id));
@@ -84,6 +84,5 @@ public class StudentRegistrationController {
                 .collect(Collectors.toList());
         return new ResponseEntity<>(new ApiResponse(HttpStatus.OK,dtoList), HttpStatus.OK);
     }
-
 }
 
