d1e7b0e3cf968a0120e21ad987b4a6c1ee5db997
ShreeJananiMK <124903285+ShreeJananiMK@users.noreply.github.com>
2025-05-04 17:14:20 +0530

    Update StudentRegistrationCount.java

--- loginportal/loginportal/loginportal/src/main/java/com/snr/loginportal/dto/StudentRegistrationCount.java
+++ loginportal/loginportal/loginportal/src/main/java/com/snr/loginportal/dto/StudentRegistrationCount.java
@@ -1,1 +1,38 @@
+package com.snr.loginportal.dto;
+
+public class StudentRegistrationCount {
+
+    private String eventName;
+
+    private Long count;
+
+    public StudentRegistrationCount(String eventName, Long count) {
+        this.eventName = eventName;
+        this.count = count;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public Long getCount() {
+        return count;
+    }
+
+    public void setCount(Long count) {
+        this.count = count;
+    }
+
+    @Override
+    public String toString() {
+        return "StudentRegistrationCount{" +
+                "eventName='" + eventName + '\'' +
+                ", count='" + count + '\'' +
+                '}';
+    }
+}
 
